3472ff936b5d384add66d74471a4a9090066ae32
jquinnie <jenn8866@gmail.com>
2020-04-07 16:22:50 -0600

    Updates

---
+++ precache-manifest.20812c6f416d96886aca8f514b38b9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ebe0a23c517f3512cd38c5a7984167",
+    "url": "/JQuinnie/covid-trackr/index.html"
+  },
+  {
+    "revision": "5ecf99917ae01dcdd1e2",
+    "url": "/JQuinnie/covid-trackr/static/css/main.6c95eb11.chunk.css"
+  },
+  {
+    "revision": "b840eb3363b065bc01c8",
+    "url": "/JQuinnie/covid-trackr/static/js/2.040c2247.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/JQuinnie/covid-trackr/static/js/2.040c2247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ecf99917ae01dcdd1e2",
+    "url": "/JQuinnie/covid-trackr/static/js/main.0dbdb6a5.chunk.js"
+  },
+  {
+    "revision": "191f199c1ea6622334a2",
+    "url": "/JQuinnie/covid-trackr/static/js/runtime-main.59702c45.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/JQuinnie/covid-trackr/static/media/image.d7265326.png"
+  }
+]);
